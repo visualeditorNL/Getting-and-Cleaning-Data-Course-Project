ef5dd53192789ed22c32769372b452f953332978
visualeditor.NL <marijnvandermeer@me.com>
2015-02-12 22:44:32 +0100

    first start

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,5 @@
 #here comes my assessment
+
+working_dir <- getwd()
+filenames <- list.files(paste0(working_dir,"/UCI HAR Dataset"))
+test <- do.call("rbind", lapply(filenames, read.csv, sep = ";", header = TRUE))
